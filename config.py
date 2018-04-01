3f7d35c7f4dea189a5dc775e03cfaa317054dbc5
RAhulKUmaWat95 <37971028+RAhulKUmaWat95@users.noreply.github.com>
2018-04-01 11:17:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+LTM_DATA_TABLE = "ltm_data"
+LAST_TIME_MOVED = "last_time_moved"
+
+#TODO CONFIGURE db path
+DB_PATH = r"/home/pi/tribehacks/ltm_db.json"
+
+#TODO Configure Real Server ip and port
+HOST = "127.0.0.1"
+PORT = 8000
+UTF_FORMAT = "UTF-8"
+© 2018 GitHub, Inc.
